d004187f8308e860b6284ac188f7b952e9ed5c5c
Sue Mu <mushuyun@yahoo.com>
2020-08-28 16:38:16 -0400

    Updates

---
+++ precache-manifest.e6235feea43cb4aee1e12cdf917be913.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9534cb73e840084826633d769eebc9",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "6a5beca6d21bc123a780",
+    "url": "/monster/static/css/main.91eff281.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monster/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a5beca6d21bc123a780",
+    "url": "/monster/static/js/main.4e3054fc.chunk.js"
+  },
+  {
+    "revision": "562e33ad7bf5e068a5dc",
+    "url": "/monster/static/js/runtime-main.745f0dd4.js"
+  }
+]);
